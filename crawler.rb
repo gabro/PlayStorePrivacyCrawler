0be8bb1021ee7ffe6e066cd88133d01ac9d3a780
Gabriele Petronella <gabriele.petronella@gmail.com>
2013-10-22 18:31:17 -0500

    2-space indentation instead of tab

--- crawler.rb
+++ crawler.rb
@@ -26,87 +26,87 @@ xmark = "\u2718".red
 circle = "\u25CB".white
 
 time = Benchmark.realtime do
-	## Validate arguments
-	unless ARGV.length == 1
-	  puts "Wrong number of arguments. Expected 1, given #{ARGV.length}"
-	  abort
-	end
+  ## Validate arguments
+  unless ARGV.length == 1
+    puts "Wrong number of arguments. Expected 1, given #{ARGV.length}"
+    abort
+  end
 
-	## Build Play Store URL
-	package_name = ARGV.first.chomp
-	play_store_url = "https://play.google.com/store/apps/details?id=#{package_name}"
-	
-	## Search for "Privacy Policy" link in Play Store
-	privacy_url = nil
-	time = Benchmark.realtime do
-		browser = Mechanize.new
-		browser.follow_meta_refresh = true
-		browser.get(play_store_url) do |page|
-			privacy_link = page.link_with(:text => / Privacy Policy /)
-			unless privacy_link.nil?
-				begin
-					privacy_page = browser.click(privacy_link)
-					privacy_url = privacy_page.uri.to_s
-				rescue Mechanize::ResponseCodeError => e
-					puts "#{checkmark} Searched Play Store for privacy policy #{time}"
-					puts "    #{xmark} Found URL #{e.page.uri.to_s.underline.light_red} but it appears to be invalid"
-					abort
-				end
-			end
-		end
-	end
-	time = "(#{time} s)".cyan
-	puts "#{checkmark} Searched Play Store for privacy policy #{time}"
+  ## Build Play Store URL
+  package_name = ARGV.first.chomp
+  play_store_url = "https://play.google.com/store/apps/details?id=#{package_name}"
+  
+  ## Search for "Privacy Policy" link in Play Store
+  privacy_url = nil
+  time = Benchmark.realtime do
+    browser = Mechanize.new
+    browser.follow_meta_refresh = true
+    browser.get(play_store_url) do |page|
+      privacy_link = page.link_with(:text => / Privacy Policy /)
+      unless privacy_link.nil?
+        begin
+          privacy_page = browser.click(privacy_link)
+          privacy_url = privacy_page.uri.to_s
+        rescue Mechanize::ResponseCodeError => e
+          puts "#{checkmark} Searched Play Store for privacy policy #{time}"
+          puts "    #{xmark} Found URL #{e.page.uri.to_s.underline.light_red} but it appears to be invalid"
+          abort
+        end
+      end
+    end
+  end
+  time = "(#{time} s)".cyan
+  puts "#{checkmark} Searched Play Store for privacy policy #{time}"
 
-	if privacy_url.nil?
-		puts "    #{xmark} No privacy policy found for #{package_name.yellow}"
-		abort
-	end
-	puts "    #{checkmark} Found privacy policy for #{package_name.yellow} at #{privacy_url.underline.light_red}"
+  if privacy_url.nil?
+    puts "    #{xmark} No privacy policy found for #{package_name.yellow}"
+    abort
+  end
+  puts "    #{checkmark} Found privacy policy for #{package_name.yellow} at #{privacy_url.underline.light_red}"
 
-	## Download privacy policy document
-	privacy_doc = nil
-	time = Benchmark.realtime do
-		begin
-			Timeout.timeout(10) do
-				privacy_doc = document privacy_url
-			end
-		rescue Timeout::Error, Treat::Exception
-			puts "#{xmark} A timeout error occurred while downloading the privacy policy from #{privacy_url.underline.light_red}"
-			abort
-		end
-	end
-	time = "(#{time} s)".cyan
-	puts "#{checkmark} Downloaded privacy policy #{time}"
+  ## Download privacy policy document
+  privacy_doc = nil
+  time = Benchmark.realtime do
+    begin
+      Timeout.timeout(10) do
+        privacy_doc = document privacy_url
+      end
+    rescue Timeout::Error, Treat::Exception
+      puts "#{xmark} A timeout error occurred while downloading the privacy policy from #{privacy_url.underline.light_red}"
+      abort
+    end
+  end
+  time = "(#{time} s)".cyan
+  puts "#{checkmark} Downloaded privacy policy #{time}"
 
-	## Parse privacy policy document
-	time = Benchmark.realtime do
-		privacy_doc.apply :chunk, :segment, :tokenize
-	end
-	time = "(#{time} s)".cyan
-	puts "#{checkmark} Parsed privacy policy #{time}"
+  ## Parse privacy policy document
+  time = Benchmark.realtime do
+    privacy_doc.apply :chunk, :segment, :tokenize
+  end
+  time = "(#{time} s)".cyan
+  puts "#{checkmark} Parsed privacy policy #{time}"
 
-	## Export .dot file
-	time = Benchmark.realtime do
-		privacy_doc.visualize :dot, file: "#{package_name}.dot"
-	end
-	time = "(#{time} s)".cyan
-	puts "#{checkmark} Generated .dot file #{time}"
+  ## Export .dot file
+  time = Benchmark.realtime do
+    privacy_doc.visualize :dot, file: "#{package_name}.dot"
+  end
+  time = "(#{time} s)".cyan
+  puts "#{checkmark} Generated .dot file #{time}"
 
-	if which('dot').nil?
-		dot = "dot".yellow
-		puts "#{circle} Skipping .pdf generation as #{dot} is not installed"
-	else
-		## Export .pdf file
-		time = Benchmark.realtime do
-			`dot -o #{package_name}.pdf -Tpdf #{package_name}.dot`
-		end
-		time = "(#{time} s)".cyan
-		puts "#{checkmark} Generated .pdf file #{time}"
+  if which('dot').nil?
+    dot = "dot".yellow
+    puts "#{circle} Skipping .pdf generation as #{dot} is not installed"
+  else
+    ## Export .pdf file
+    time = Benchmark.realtime do
+      `dot -o #{package_name}.pdf -Tpdf #{package_name}.dot`
+    end
+    time = "(#{time} s)".cyan
+    puts "#{checkmark} Generated .pdf file #{time}"
 
-		## Open .pdf file
-		`open #{package_name}.pdf`
-	end
+    ## Open .pdf file
+    `open #{package_name}.pdf`
+  end
 end
 
 ## Final stats
